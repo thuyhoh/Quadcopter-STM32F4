2e5aa46e2878e9946b0ea1572e3a2c0f6e8e732e
Nguyen Trong Thuy <tembedded2004@gmail.com>
2026-08-17 10:09:00 +0700

    Fly controller

---
+++ STM32-Fly_Controller/Core/Inc/main.h
@@ -0,0 +1,95 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define BUZZER_Pin GPIO_PIN_14
+#define BUZZER_GPIO_Port GPIOC
+#define ESC1_Pin GPIO_PIN_0
+#define ESC1_GPIO_Port GPIOA
+#define ESC2_Pin GPIO_PIN_1
+#define ESC2_GPIO_Port GPIOA
+#define ESC3_Pin GPIO_PIN_2
+#define ESC3_GPIO_Port GPIOA
+#define ESC4_Pin GPIO_PIN_3
+#define ESC4_GPIO_Port GPIOA
+#define VOLTAGE_IN_Pin GPIO_PIN_4
+#define VOLTAGE_IN_GPIO_Port GPIOA
+#define NRF24_CE_Pin GPIO_PIN_0
+#define NRF24_CE_GPIO_Port GPIOB
+#define NRF24_IRQ_Pin GPIO_PIN_1
+#define NRF24_IRQ_GPIO_Port GPIOB
+#define NRF24_CSN_Pin GPIO_PIN_2
+#define NRF24_CSN_GPIO_Port GPIOB
+#define GPS_TX_Pin GPIO_PIN_9
+#define GPS_TX_GPIO_Port GPIOA
+#define GPS_RX_Pin GPIO_PIN_10
+#define GPS_RX_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ STM32-Fly_Controller/Debug/Devices/nrf24l01/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Devices/nrf24l01/nrf24l01.c 
+
+OBJS += \
+./Devices/nrf24l01/nrf24l01.o 
+
+C_DEPS += \
+./Devices/nrf24l01/nrf24l01.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Devices/nrf24l01/%.o Devices/nrf24l01/%.su Devices/nrf24l01/%.cyclo: ../Devices/nrf24l01/%.c Devices/nrf24l01/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"/home/thuy/embedded/project/Quadcopter-STM32F4/STM32-Fly_Controller/Devices/nrf24l01" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Devices-2f-nrf24l01
+
+clean-Devices-2f-nrf24l01:
+	-$(RM) ./Devices/nrf24l01/nrf24l01.cyclo ./Devices/nrf24l01/nrf24l01.d ./Devices/nrf24l01/nrf24l01.o ./Devices/nrf24l01/nrf24l01.su
+
+.PHONY: clean-Devices-2f-nrf24l01
+
